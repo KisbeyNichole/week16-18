f54a5843a68ee47f647eb26e9f42dd9ecc95a58a
Nichole <kisbeynichole@gmail.com>
2024-08-28 13:22:57 -0700

    placeholder for Amissara

--- final/src/pages/Amissara.js
+++ final/src/pages/Amissara.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
+import AmissaraImage from '../images/Amissara.png'
 
 export default function amissara() {
   return (
-    <div><NavBar /></div>
+    <div><NavBar />
+    <img width="100%" src={AmissaraImage} />
+    </div>
   )
 }
